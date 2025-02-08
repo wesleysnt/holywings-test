2506520d47dde9e5c8a3f8073d3492a448b2d6d4
Wesley Santana <wesley.santana@redtech.co.id>
2025-02-08 11:45:34 +0700

    feat:update book category

---
+++ app/Http/Requests/api/CreateBookCategoryRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\api;
+
+use App\Helpers\ResponseHelper;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateBookCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category'=>"required"
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->all();
+
+        // Throw the custom response using ResponseHelper
+        throw new HttpResponseException(
+            ResponseHelper::error(
+                'Validation failed',
+                422,
+                ['errors' => $errors]
+            )
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\BookCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,8 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login',[AuthController::class,'Login']);
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('book-category', BookCategoryController::class);
 });
